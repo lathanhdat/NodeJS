e0ddabc171c873db06c813ef35591b651243d058
dat <datla@gmail.com>
2018-10-31 15:37:48 +0700

    Origin

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = 
+{
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => x*y
+};
+function solveRect(l,b)
+{
+    console.log("Solving for rectangle with l = " + l + "and b = " + b);
+    if(l <= 0 || b <= 0)
+    {
+        console.log("Dimistion < 0");
+    }
+    else
+    {
+        console.log("Area = " + rect.area(l,b));
+        console.log("Perimeter = " + rect.perimeter(l,b));
+    }
+}
+solveRect(2,4);
+solveRect(3,5);
